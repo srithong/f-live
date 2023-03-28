02f465c44fd1799f8ba15274051543c2c0901862
song <heyboonsong@gmail.com>
2023-03-28 20:05:26 +0700

    change api

--- api/index.js
+++ api/index.js
@@ -9,8 +9,9 @@ function start() {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cors());
 
+  let monitors = [];
   let clients = [];
-  let comments = [];
+  let messages = [];
 
   app.get("/api/live", (req, res) => {
     const name = req.query.name;
@@ -23,53 +24,72 @@ function start() {
     };
     res.writeHead(200, headers);
     const id = uuidv4();
-    clients = [...clients, { res, id }];
-    comments = [
-      ...comments,
+    monitors = [...monitors, { res, id }];
+    messages = [
+      ...messages,
       {
-        name: "",
+        name: "System",
         message: `${name} joined the live stream`,
         date: new Date(),
       },
     ];
-    clients.forEach((client) => {
+    monitors.forEach((client) => {
       const eventName = `event: live\n`;
-      const data = `data: ${JSON.stringify(comments)}\n\n`;
+      const data = `data: ${JSON.stringify(messages)}\n\n`;
       client.res.write(eventName);
       client.res.write(data);
     });
     req.on("close", () => {
-      clients = clients.filter((client) => client.id !== id);
+      monitors = monitors.filter((client) => client.id !== id);
       console.log("lost connection");
     });
   });
 
-  app.post("/api/comments", (req, res) => {
-    const { name, message } = req.body;
-    if (!name || !message)
-      return res.status(400).send("name and message are required");
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    comments = [
-      ...comments,
+  app.post("/api/messages", (req, res) => {
+    const { uuid, message } = req.body;
+    if (!uuid || !message)
+      return res.status(400).send("uuid and message are required");
+    const client = clients.find((client) => client.uuid === uuid);
+    if (!client) return res.status(400).send("invalid uuid");
+
+    messages = [
+      ...messages,
       {
-        name: name,
-        color: "#" + randomColor,
+        name: client.name,
+        uuid: uuid,
+        color: "#" + Math.floor(Math.random() * 16777215).toString(16),
         message: message,
         date: new Date(),
       },
     ];
+
     clients.forEach((client) => {
       const eventName = `event: live\n`;
-      const data = `data: ${JSON.stringify(comments)}\n\n`;
+      const data = `data: ${JSON.stringify(messages)}\n\n`;
       client.res.write(eventName);
       client.res.write(data);
     });
     res.sendStatus(200);
   });
 
+  app.get("/api/messages", (req, res) => {
+    res.status(200).send(messages);
+  });
+
+  app.post("/api/register", (req, res) => {
+    const { name } = req.body;
+    if (!name) return res.status(400).send("name is required");
+    const uuid = uuidv4();
+    clients = [...clients, { name, uuid }];
+    res.status(200).send({
+      uuid,
+    });
+  });
+
   setInterval(() => {
-    comments = [];
+    messages = [];
   }, 600000);
+
   app.listen(3000, () => console.log("Example app listening on port 3000!"));
 }
 
